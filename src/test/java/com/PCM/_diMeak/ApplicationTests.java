98d1f475e0972491232315c9bb169a6d04edee3d
jad-png <j.belassiria3993@uca.ac.ma>
2025-11-18 15:31:35 +0100

    initialize spring project

---
+++ src/test/java/com/PCM/_diMeak/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PCM._diMeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
