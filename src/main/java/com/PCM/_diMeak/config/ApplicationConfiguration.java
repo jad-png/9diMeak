7f3bc7c309c64d0615bfafcedb271639b362d6cb
jad-png <j.belassiria3993@uca.ac.ma>
2025-12-19 10:59:14 +0100

    configure Spring Security with UserDetailsService, BCrypt encoder, and authentication beans

---
+++ src/main/java/com/PCM/_diMeak/config/ApplicationConfiguration.java
@@ -0,0 +1,47 @@
+package com.PCM._diMeak.config;
+
+import com.PCM._diMeak.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration {
+    private final UserRepository userRepo;
+
+    public ApplicationConfiguration(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder psswrdEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(psswrdEncoder());
+
+        return authProvider;
+    }
+}
